ec4197c7e1947044d534e7848cc69bc9215b2ad7
EndrisMoh <endrismd@gmail.com>
2023-04-25 19:29:39 +0000

    Basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,12 @@
+/**
+ * Create a function named getListStudents that returns an array of objects.
+ * Each object should have three attributes: 
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
