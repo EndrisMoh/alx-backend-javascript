859a552396e17ad10dce94c5632d0f7f07ae3ca0
Endris M <EndrisMd@gmail.com>
2023-06-13 23:42:02 +0300

    Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ *  A function named displayMessage that
+ *  Displays a message on the STDOUT string argument.
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
